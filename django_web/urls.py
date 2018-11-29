75be9515e235d54cdda503aef5e5218ac6252bd7
iIQOverflow <fengjianyongcn@gmail.com>
2018-11-29 10:14:56 +0800

    (branch:api_design) rest framework installation

--- django_web/urls.py
+++ django_web/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
+    #    path(r'^api-auth/'), include('rest_framework.urls'),
 ]
