c21890efe46871540882c9b7749f138f343dcc07
iIQOverflow <fengjianyongcn@gmail.com>
2018-11-29 14:31:37 +0800

    (branch: api_design) example

--- django_web/settings.py
+++ django_web/settings.py
@@ -129,3 +129,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# rest framework
+REST_FRAMEWORK = {
+    # Use Django's standard 'django.contrib.auth' permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
